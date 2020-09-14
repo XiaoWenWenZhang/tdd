22b9db64bea3052712972cfd4b7460d21ee02a61
xiaowen <846703636@qq.com>
2020-09-14 11:53:11 +0800

    Implemented return an initial string when given an string which numbers of vowels not more than 30%

--- src/test/java/Mommifier.java
+++ src/test/java/Mommifier.java
@@ -1,6 +1,10 @@
+
+
 public class Mommifier {
 
     public String convert(String s) {
         return s;
     }
+
+
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -6,10 +6,16 @@ public class MommifierTest {
     //如果string不符合要求
     //如果string符合要求
     @Test
-    void should_return_initial_string_when_convert_given_string_is_empty(){
+    void should_return_empty_string_when_convert_given_string_is_empty(){
         Mommifier mommifier =new Mommifier();
         String result = mommifier.convert("");
         assertEquals("", result);
+    }
 
+    @Test
+    void should_return_initial_string_when_convert_given_number_of_vowels_not_more_than_30percent(){
+        Mommifier mommifier =new Mommifier();
+        String result = mommifier.convert("ahyphjkslxzmn");
+        assertEquals("ahyphjkslxzmn", result);
     }
 }
